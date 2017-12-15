fdc2569fc452ea6f240f2c89f60b97be033c6b20
alex <crossroadsman@gmail.com>
2017-12-15 10:48:31 -0700

    Modify capcha to handle part two

--- 2017/01/inverse_captcha.py
+++ 2017/01/inverse_captcha.py
@@ -21,18 +21,32 @@ class InverseCaptcha():
     def listify(self, n: int) -> [int]:
         return [int(i) for i in str(n)]
 
-    def sumDigits(self, l: [int]) -> int:
+    def next_digit(self, i: int, l: [int], mode: int=0) -> int:
+        '''modes: 0 - next digit (circular)
+                  1 - halfway round (circular)'''
+        if len(l) <= 1:
+            return None
+
+        next_i = None
+        if mode == 0:
+            next_i = (i + 1) % len(l)
+
+        elif mode == 1:
+            hop_length = len(l) // 2
+            next_i = (i + hop_length) % len(l)
+
+        return next_i
+
+    def sumDigits(self, l: [int], mode: int=0) -> int:
+
         if len(l) <= 1:
             return 0
 
         running_total = 0
         for i, v in enumerate(l):
-            if i == len(l) - 1:
-                if self.isSame(v, l[0]):
-                    running_total += v
-            else:
-                if self.isSame(v, l[i+1]):
-                    running_total += v
+            next_i = self.next_digit(i=i, l=l, mode=mode)
+            if v == l[next_i]:
+                running_total += v
 
         return running_total
 
