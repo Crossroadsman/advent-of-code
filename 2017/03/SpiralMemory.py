8f9336f228a2382afd2c4b0795be6773d900e81d
Alex Koumparos <crossroadsman@gmail.com>
2017-12-16 23:59:47 -0700

    Update SpiralMemory.py

--- 2017/03/SpiralMemory.py
+++ 2017/03/SpiralMemory.py
@@ -2,7 +2,19 @@
 Day 3: Spiral Memory
 --------------------
 
-level 1: 0 (1) (2 ^ 0)
-level 2: 1–9 (8) (2 ^ 3)
-level 3: 10–25 (16) (2 ^ 4)
-level 4: 26–
+level 1: 1 (1) --- 8(n - 1)
+level 2: 2–9 (8) --- 8(n - 1)
+level 3: 10–25 (16) --- 
+level 4: 26–49 (24)
+
+At each level the size of the ring is 8(n-1).
+The cumulative elements is:
+
+L1 – 1 1^2
+L2 – 9 3^2
+L3 – 25 5^2
+L4 – 49 7^2
+
+cumulative elements at each level is (2n-1)^2
+
+so for a given number, say
