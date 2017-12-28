3bd75ae6bcde42bb8ac3964cd5db4767196db76e
alex <crossroadsman@gmail.com>
2017-12-27 22:19:49 -0700

    Fix syntax errors

--- 2017/06/memory_allocation.py
+++ 2017/06/memory_allocation.py
@@ -39,9 +39,9 @@ that has been seen before?
 class MemoryAllocation():
 
     def index_max(self, arr: [int]) -> int:
-        return index(max(arr))
+        return arr.index(max(arr))
     
-    def solver(arr: [int]) -> int:
+    def solver(self, arr: [int]) -> int:
         '''Takes an array of ints, reallocates the elements as described at the top of the module. This process is repeated until a
            reallocation produces a value that has been seen before, at which point the number of iterations (including the final one)
            is returned.'''
@@ -49,13 +49,13 @@ class MemoryAllocation():
         working_list = arr[:] # copy the input list into a working version
         history = [working_list] # add the initial state to the history list
         
-        while true:
-            i = index_max(arr: working_list)
+        while True:
+            i = self.index_max(arr=working_list)
             num_to_allocate = working_list[i]
             
             while num_to_allocate > 0:
                 i = (i + 1) % len(working_list)
-                working_list(i) += 1
+                working_list[i] += 1
                 num_to_allocate -= 1
                 
             if working_list in history:
