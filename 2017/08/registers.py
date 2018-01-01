034cafc35519ecdc31eadccbab9ac89836a3f010
Alex Koumparos <crossroadsman@gmail.com>
2017-12-31 23:53:14 -0700

    Update registers.py

--- 2017/08/registers.py
+++ 2017/08/registers.py
@@ -33,3 +33,67 @@ the registers are named, and leaves that to you to determine.
 
 What is the largest value in any register after completing the instructions in your puzzle input?
 '''
+import re
+
+class CleanData():
+    
+    def get_filename(self):
+        return input("Enter the filename to open > ")
+
+    def clean_data(self, filename: str) -> [{str:str}]:
+        file_object = open(filename, 'r')
+        
+        cleaned_data = []
+        '''
+        (?P<register>\w+)[ \t]+(?P<instruction>\w+)[ \t]+(?P<quantum>[-\d]+)[ \t]+(if (?P<query_register>\w+)[ \t]+(?P<query_operator>[!=><]+)[ \t]+(?P<query_value>[-\d]+))?
+        Named Capture Group `register` (?P<register>\w+)
+            \w+ matches any word character (equal to [a-zA-Z0-9_])
+            + Quantifier — Matches between one and unlimited times, as many times as possible, giving back as needed (greedy)
+        Match a single character present in the list below [ \t]+
+            + Quantifier — Matches between one and unlimited times, as many times as possible, giving back as needed (greedy)
+              matches the character   literally (case sensitive)
+            \t matches a tab character (ASCII 9)
+        Named Capture Group instruction (?P<instruction>\w+)
+            \w+ matches any word character (equal to [a-zA-Z0-9_])
+            + Quantifier — Matches between one and unlimited times, as many times as possible, giving back as needed (greedy)
+        Match a single character present in the list below [ \t]+
+            + Quantifier — Matches between one and unlimited times, as many times as possible, giving back as needed (greedy)
+              matches the character   literally (case sensitive)
+            \t matches a tab character (ASCII 9)
+        Named Capture Group quantum (?P<quantum>[-\d]+)
+            Match a single character present in the list below [-\d]+
+            + Quantifier — Matches between one and unlimited times, as many times as possible, giving back as needed (greedy)
+            - matches the character - literally (case sensitive)
+            \d matches a digit (equal to [0-9])
+        Match a single character present in the list below [ \t]+
+            + Quantifier — Matches between one and unlimited times, as many times as possible, giving back as needed (greedy)
+              matches the character   literally (case sensitive)
+            \t matches a tab character (ASCII 9)
+        4th Capturing Group (if (?P<query_register>\w+)[ \t]+(?P<query_operator>[!=><]+)[ \t]+(?P<query_value>[-\d]+))?
+            ? Quantifier — Matches between zero and one times, as many times as possible, giving back as needed (greedy)
+            if  matches the characters if  literally (case sensitive)
+            Named Capture Group query_register (?P<query_register>\w+)
+                Match a single character present in the list below [ \t]+
+            Named Capture Group query_operator (?P<query_operator>[!=><]+)
+                Match a single character present in the list below [ \t]+
+            Named Capture Group query_value (?P<query_value>[-\d]+)
+        '''
+        pattern = (?P<register>\w+)[ \t]+(?P<instruction>\w+)[ \t]+(?P<quantum>[-\d]+)[ \t]+(if (?P<query_register>\w+)[ \t]+(?P<query_operator>[!=><]+)[ \t]+(?P<query_value>[-\d]+))?
+        
+        for line in file_object:
+
+            match_object = re.match(pattern, line)
+            
+            if match_object: # a match was found
+                line_data = {'register': match_object.groupdict()['register'],
+                             'instruction': match_object.groupdict()['instruction'],
+                             'quantum': match_object.groupdict()['quantum'],
+                             'query_register': match_object.groupdict()['query_register'],
+                             'query_operator': match_object.groupdict()['query_operator'],
+                             'query_value': match_object.groupdict()['query_value'],
+                             }
+                cleaned_data.append(line_data)
+                
+            else: # line didn't match the regex -> malformed input?
+                print("line did not conform to expected format")
+                
