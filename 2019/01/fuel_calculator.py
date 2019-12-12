b30bca54f865e8e3c1e446e358b75e46851190bf
CrossroadsMan <crossroadsman@gmail.com>
2019-12-12 12:28:33 -0700

    Complete day 1

--- 2019/01/fuel_calculator.py
+++ 2019/01/fuel_calculator.py
@@ -1,9 +1,18 @@
 from file_handling import file_to_list, strs_to_ints
 
 
-def calculate_from_mass(mass: int) -> int:
-    fuel_required = (mass // 3) - 2
-    return fuel_required
+def calculate_from_mass(mass: int, fuel_so_far: int=0) -> int:
+    """recursive computation because fuel also has mass"""
+
+    fuel_from_mass = (mass // 3) - 2
+
+    if fuel_from_mass < 0:
+       return fuel_so_far
+
+    return calculate_from_mass(
+        mass=fuel_from_mass,
+        fuel_so_far=fuel_so_far + fuel_from_mass
+    )
 
 
 def calculate_from_modules(modules: [int]) -> int:
